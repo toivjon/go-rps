072d10b71055f62ecafa7e8ec5398ec42090fe15
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-10-29 21:24:11 +0300

    Fixed the bin path for the build.sh script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ ROOTPATH=$(dirname $(readlink -f $0))
 ROOTPATH=$(dirname $ROOTPATH)
 
 # Specify the folder where to put build results.
-BINPATH="$ROOTPATH/bin/server"
+BINPATH="$ROOTPATH/bin"
 
 # Clear the old binary folder by re-creating it.
 [ -d $BINPATH ] && rm -rf $BINPATH
@@ -14,8 +14,8 @@ mkdir -p $BINPATH
 
 # Build the binaries.
 printf "Building the binaries. Please wait...\n"
-go build -o $BINPATH $ROOTPATH\cmd\server
-go build -o $BINPATH $ROOTPATH\cmd\client
+go build -o $BINPATH $ROOTPATH/cmd/server
+go build -o $BINPATH $ROOTPATH/cmd/client
 
 # Show information related to compilation.
 printf "Build succeeded:\n"
