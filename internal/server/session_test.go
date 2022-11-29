9d277c564753aa957bbfdbcaa6f846ba4a7ca99c
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-11-29 21:28:25 +0200

    Added a stub for session unit tests.

---
+++ internal/server/session_test.go
@@ -0,0 +1,20 @@
+package server_test
+
+import (
+	"testing"
+)
+
+func TestNewSession(t *testing.T) {
+	t.Parallel()
+	// ...
+}
+
+func TestSessionStart(t *testing.T) {
+	t.Parallel()
+	// ...
+}
+
+func TestSessionClose(t *testing.T) {
+	t.Parallel()
+	// ...
+}
