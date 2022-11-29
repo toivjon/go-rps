f6281df7123bafa0bd45e8345082b827244902a8
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-11-29 21:20:59 +0200

    Minor cleanup for the session identifiers.

--- internal/server/session.go
+++ internal/server/session.go
@@ -13,23 +13,23 @@ type session struct {
 }
 
 func (s *session) Start() error {
-	log.Printf("Session %p starting...", s)
+	log.Printf("Session %#p starting...", s)
 	if err := com.WriteMessage(s.player1.Conn, com.TypeStart, com.StartContent{OpponentName: s.player2.Name}); err != nil {
-		return fmt.Errorf("failed to write start message for client %p. %w", s.player1, err)
+		return fmt.Errorf("failed to write start message for client %#p. %w", s.player1, err)
 	}
 	if err := com.WriteMessage(s.player2.Conn, com.TypeStart, com.StartContent{OpponentName: s.player1.Name}); err != nil {
-		return fmt.Errorf("failed to write start message for client %p. %w", s.player2, err)
+		return fmt.Errorf("failed to write start message for client %#p. %w", s.player2, err)
 	}
 	go runRound(s.player1, s.player2)
-	log.Printf("Session %p started.", s)
+	log.Printf("Session %#p started.", s)
 	return nil
 }
 
 func (s *session) Close() {
-	log.Printf("Closing session %p: %q vs. %q", s, s.player1.Name, s.player2.Name)
+	log.Printf("Closing session %#p: %q vs. %q", s, s.player1.Name, s.player2.Name)
 	s.player1.Conn.Close()
 	s.player2.Conn.Close()
-	log.Printf("Closing session %p: %q vs. %q completed.", s, s.player1.Name, s.player2.Name)
+	log.Printf("Closing session %#p: %q vs. %q completed.", s, s.player1.Name, s.player2.Name)
 }
 
 func newSession(player1, player2 *Player) *session {
