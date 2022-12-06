b54e070f5491da1fe6dd4361d77e854f8728fc96
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2022-12-06 19:09:18 +0200

    Removed the unnecessary mutex from the matchmaker.

--- internal/server/matchmaker.go
+++ internal/server/matchmaker.go
@@ -2,17 +2,13 @@ package server
 
 import (
 	"log"
-	"sync"
 )
 
 type matchmaker struct {
 	waitingPlayer *Player
-	mutex         sync.Mutex
 }
 
 func (m *matchmaker) Enter(player *Player) error {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
 	log.Printf("Player %#p (%s) enters matchmaker.", player, player.Name)
 	if m.waitingPlayer == nil {
 		m.waitingPlayer = player
@@ -27,8 +23,6 @@ func (m *matchmaker) Enter(player *Player) error {
 }
 
 func (m *matchmaker) Leave(player *Player) {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
 	if m.waitingPlayer == player {
 		m.waitingPlayer = nil
 		log.Printf("Player %#p (%s) left matchmaker.", &player, player.Name)
